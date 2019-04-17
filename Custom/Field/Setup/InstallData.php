f93773acae8c3faf0344ca43a0f72b8f270df1cb
admin <megha.gwec@gmail.com>
2019-04-17 18:03:24 +0530

    add custom fields on registration page

---
+++ Custom/Field/Setup/InstallData.php
@@ -0,0 +1,72 @@
+<?php
+namespace Custom\Field\Setup;
+
+use Magento\Customer\Model\Customer;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements \Magento\Framework\Setup\InstallDataInterface
+{
+    private $eavSetupFactory;
+    
+    private $eavConfig;
+    
+    private $attributeResource;
+    
+    public function __construct(
+        \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory,
+        \Magento\Eav\Model\Config $eavConfig,
+        \Magento\Customer\Model\ResourceModel\Attribute $attributeResource
+    ) {
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->eavConfig = $eavConfig;
+        $this->attributeResource = $attributeResource;
+    }
+    
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        $eavSetup->removeAttribute(Customer::ENTITY, "skype");
+
+        $attributeSetId = $eavSetup->getDefaultAttributeSetId(Customer::ENTITY);
+        $attributeGroupId = $eavSetup->getDefaultAttributeGroupId(Customer::ENTITY);
+
+        $eavSetup->addAttribute(Customer::ENTITY, 'skype', [
+            // Attribute parameters
+            'type' => 'varchar',
+            'label' => 'Skype Account',
+            'input' => 'text',
+            'required' => true,
+            'visible' => true,
+            'user_defined' => true,
+            'sort_order' => 990,
+            'position' => 990,
+            'system' => 0,
+        ]);
+        
+        $attribute = $this->eavConfig->getAttribute(Customer::ENTITY, 'skype');
+        $attribute->setData('attribute_set_id', $attributeSetId);
+        $attribute->setData('attribute_group_id', $attributeGroupId);
+
+        /*
+        //You can use this attribute in the following forms
+        adminhtml_checkout
+        adminhtml_customer
+        adminhtml_customer_address
+        customer_account_create
+        customer_account_edit
+        customer_address_edit
+        customer_register_address
+        */
+
+        $attribute->setData('used_in_forms', [
+            'adminhtml_customer',
+            'customer_account_create',
+            'customer_account_edit'
+        ]);
+
+        $this->attributeResource->save($attribute);
+    }
+}
+?>
